a8690bc8e26bf3c4f54bab9df7f6b21a4bccba67
Meiram <sapsan39@gmail.com>
2021-08-27 23:15:24 +0600

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module'
 
 import { AppComponent } from './app.component';
+import { PatientListComponent } from './patient-list/patient-list.component';
+import { PatientService } from './patient.service';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NoopAnimationsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [PatientService]
 })
 export class AppModule { }
